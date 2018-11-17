77dd00c2d767ed59da817d661e8ccbcf76ec6f94
Glitch (sapphire-tornado) <none>
2018-11-17 16:59:02 +0000

    🏠🎩 Checkpoint ./package.json:275522/1258 ./routes/api.js:275522/415

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,17 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
+const googleStocks = require('google-stocks');
+ 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(async (req, res) => {
+      const stocks = await googleStocks(['AAPL'])
       res.json({
         stockData: { stock: "GOOG", price: "786.90", likes: 1 } 
       })
